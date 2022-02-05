e0aaad54682210b2576b8528f30306d7785d8797
Doum Akono Rudolph <doumakono@gmail.com>
2022-02-05 11:23:14 +0100

    Test de la fonction login

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class LoginController extends AbstractController
+{
+    /**
+     * @Route("/api/security/login", name="api-login", methods={"POST"})
+     */
+    public function index(Request $request): Response
+    {
+        $username = $request->request->get("username");
+        $password = $request->request->get("password");
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        $response->setContent(json_encode("$username + ' ' + $password"));
+
+        return $response;
+    }
+}
